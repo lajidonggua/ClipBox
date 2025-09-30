0ee2660f2b76be617f94f869220a175764d713c2
lajidonggua <dongguaxiami@gmail.com>
2025-09-30 10:26:48 +0800

    fix: lang issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    println!("LANG = {:?}", std::env::var("LANG"));
+    println!("LC_ALL = {:?}", std::env::var("LC_ALL")); 
+    // 确保双击 .app 时也能用 UTF-8
+    std::env::set_var("LANG", "zh_CN.UTF-8");
+    std::env::set_var("LC_ALL", "zh_CN.UTF-8");
+
+    println!("LANG = {:?}", std::env::var("LANG"));
+    println!("LC_ALL = {:?}", std::env::var("LC_ALL")); 
     clipbox_lib::run()
 }
